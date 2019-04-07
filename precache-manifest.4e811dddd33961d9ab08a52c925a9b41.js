782a4d8b0d43e526957495b14e8fe6e9691db247
MATT0568 <lesniewiczm@vcu.edu>
2019-04-06 22:15:35 -0400

    Updates

---
+++ precache-manifest.4e811dddd33961d9ab08a52c925a9b41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "bd7e88e89757ddd6ac92",
+    "url": "/memory-game/static/js/main.120ee2d0.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bd7e88e89757ddd6ac92",
+    "url": "/memory-game/static/css/main.29e14af5.chunk.css"
+  },
+  {
+    "revision": "cd44a19d4aa27b2320f783b9eb6f79a4",
+    "url": "/memory-game/index.html"
+  }
+];
